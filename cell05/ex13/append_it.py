5d801073eb00d12a8a4ee482b86076786359ec86
rjansuwa <68040896@kmitl.ac.th>
2025-07-23 10:08:05 +0000

    add append_it.py

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    params = sys.argv[1:]
+    if len(params) == 0:
+        print("none")
+        return
+    
+    for p in params:
+        if p.endswith("ism"):
+            continue
+        print(p + "ism")
+
+if __name__ == "__main__":
+    main()
