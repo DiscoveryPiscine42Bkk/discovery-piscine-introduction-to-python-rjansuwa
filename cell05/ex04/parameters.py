bf320828ad195282a694dfa26bb7264938f1ac95
rjansuwa <68040896@kmitl.ac.th>
2025-07-23 09:02:07 +0000

    add parameters.py

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+def main():
+    num_parameters = len(sys.argv) - 1
+    print(f"Number of parameters: {num_parameters}. ")
+
+    if __name__ == "__main__":
+        main()
